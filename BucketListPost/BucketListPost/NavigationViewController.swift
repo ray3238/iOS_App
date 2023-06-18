873f2958fb16a8168f62bb0d99c00d525031b079
ray3238 <jjmh209@gmail.com>
2023-06-18 20:29:13 +0900

    reviewApp

---
+++ BucketListPost/BucketListPost/NavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationViewController.swift
+//  BucketListPost
+//
+//  Created by 이태규 on 2023/06/18.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
