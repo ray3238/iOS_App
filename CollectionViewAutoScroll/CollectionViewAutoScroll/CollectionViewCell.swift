5592c6c34d622185911a57cae09c9c18c4dad0a7
ray3238 <jjmh209@gmail.com>
2023-09-22 23:36:27 +0900

    AppReview

--- CollectionViewAutoScroll/CollectionViewAutoScroll/CollectionViewCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CollectionViewCell.swift
-//  CollectionViewAutoScroll
-//
-//  Created by 이태규 on 2023/09/19.
-//
-
-import Foundation
--- CollectionViewAutoScroll/CollectionViewAutoScroll/CollectionViewController.swift
+++ CollectionViewAutoScroll/CollectionViewAutoScroll/CollectionViewController.swift
@@ -2,13 +2,28 @@ import SnapKit
 import Then
 import UIKit
 
-class ViewController: UIViewController {
-
+class CollectionViewController: UIViewController {
+    let button = UIButton().then {
+        $0.setTitle("남자", for: .normal)
+        $0.backgroundColor = .systemPink
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(button)
+        
+        button.snp.makeConstraints {
+//            $0.top.equalToSuperview().inset(291)
+//            $0.left.equalToSuperview().inset(52)
+//            $0.right.equalToSuperview().inset(245)
+//            $0.bottom.equalToSuperview().inset(400)
+//            $0.top.equalToSuperview().inset(40)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(63)
+            $0.height.equalTo(27)
+        }
     }
-
-
+    
+    
 }
-
